12948c0cead0b3b6afab178c3d32ce0a8ebdbadf
vikzh <vikt.anmail@gmail.com>
2025-06-01 01:48:30 +0200

    update point

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -4,6 +4,39 @@ import { readOrders, writeOrders } from '../services/orderService.js';
 import { getExtraDataAboutOrder } from '../services/tonService.js';
 const router = express.Router();
 
+
+router.post('/update-order', async (req, res) => {
+  const { orderId, userAddress, extraParams } = req.body;
+  if (!orderId || !userAddress) return res.status(400).json({ error: 'Missing fields' });
+
+  try {
+    const orders = await readOrders();
+    const index = orders.findIndex(
+      order => order.orderId == orderId && order.userAddress === userAddress
+    );
+
+    if (index === -1) {
+      return res.status(404).json({ error: 'Order not found' });
+    }
+
+    const orderExtraData = await getExtraDataAboutOrder(userAddress, orderId);
+
+    orders[index] = {
+      ...orders[index],
+      timestamp: new Date().toISOString(),
+      ...orderExtraData,
+      ...extraParams,
+    };
+
+    await writeOrders(orders);
+    res.json({ message: 'Order updated successfully' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to update order' });
+  }
+});
+
+
+
 router.post('/add-order', async (req, res) => {
   const { orderId, userAddress } = req.body;
   if (!orderId || !userAddress) return res.status(400).json({ error: 'Missing fields' });
